fe7dc7be8cad38083149c0f653dc38631b769fff
gui1ps <guilhermerobertodospassos@gmail.com>
2025-08-31 19:47:48 -0300

    chore: uncommenting and adding " '@typescript-eslint/no-unused-vars':'off', " to the eslint rules

--- Backend/afiligen-api/src/modules/auth/auth.controller.ts
+++ Backend/afiligen-api/src/modules/auth/auth.controller.ts
@@ -1,4 +1,3 @@
-// src/modules/auth/auth.controller.ts
 import { Controller, Post, Body, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
@@ -9,22 +8,19 @@ import { JwtAuthGuard } from './guards/jwt-auth.guard';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // 👉 rota para registrar um novo usuário
   @Post('register')
   async register(@Body() registerUserDto: RegisterUserDto) {
     return this.authService.register(registerUserDto);
   }
 
-  // 👉 rota para login: gera e retorna o token JWT
   @Post('login')
   async login(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
   }
 
-  // 👉 rota protegida só para teste: acessível apenas com JWT válido
   @UseGuards(JwtAuthGuard)
   @Post('profile')
   async getProfile(@Request() req) {
-    return req.user; // payload do JWT (id, email, role, etc.)
+    return req.user;
   }
 }
