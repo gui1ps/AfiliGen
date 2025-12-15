42e77be711a505f0dcbf17c65c397cfa3c647b3e
gui1ps <guilhermerobertodospassos@gmail.com>
2025-12-15 12:59:58 -0300

    chore: update CORS configuration to include production frontend URL

--- Backend/afiligen-api/src/main.ts
+++ Backend/afiligen-api/src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://afiligen-frontend:3001'],
+    origin: [
+      'http://localhost:3001',
+      'http://afiligen-frontend:3001',
+      'https://afiligen-front-end.onrender.com',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
